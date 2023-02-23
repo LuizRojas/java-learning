c2f5700b5b4d85b377b3412561e7bc1e88f61ef0
Luiz <luizfrojas@gmail.com>
2023-02-23 11:51:34 -0300

    classe carro foi atualizada, e problemas de semantica resolvidos até o momento

--- PolimorfismoDeClasses/src/carros/App.java
+++ PolimorfismoDeClasses/src/carros/App.java
@@ -2,9 +2,10 @@ package carros;
 
 public class App {
 	public static void main(String[] args) {
-		Carro carro1 = new Carro("Fiesta", "Ford", "Hatch", "branco", false, false, 2013); 
-		carro1.Ligar();
-		carro1.Acelerar();
-		carro1.ReduzirParar();
+		Carro carro1 = new Carro("Chevette", "Chevrolet", "Sedan", "prata", false, false, true, 1997); 
+		carro1.ligar();
+		carro1.acelerar();
+		carro1.reduzirParar();
+		carro1.desligar();
 	}
 }
--- PolimorfismoDeClasses/src/carros/Carro.java
+++ PolimorfismoDeClasses/src/carros/Carro.java
@@ -7,58 +7,72 @@ public class Carro {
 	private String cor;
 	private boolean ligado;
 	private boolean acelerando;
+	private boolean freioDeMao;
 	private int ano;
 	
-	public Carro(String modelo, String marca, String tipo, String cor, boolean ligado, boolean acelerando, int ano) {
+	public Carro(String modelo, String marca, String tipo, String cor, boolean ligado, boolean acelerando, boolean freioDeMao, int ano) {
 		this.modelo = modelo;
 		this.marca = marca;
 		this.tipo = tipo;
 		this.cor = cor;
 		this.ligado = ligado;
 		this.acelerando = acelerando;
+		this.freioDeMao = freioDeMao;
 		this.ano = ano;
 	}
 	
-	public void Ligar() {
+	public void vender() {}
+	
+	public void ligar() {
 		if (isLigado()) {
 			System.out.println(marca + " " + modelo + " ja esta ligado.");
 		} else {
 			setLigado(true);
-			System.out.println(marca + " " + modelo + " foi ligado.");
 		}
 	}
 	
-	public void Desligar() {
-		if (!isLigado()) {
+	public void desligar() {
+		if (isAcelerando()) {
+			System.out.println(marca + " " + modelo + " nao pode ser desligado em movimento.");
+		} else if (!isLigado()) {
 			System.out.println(marca + " " + modelo + " ja esta desligado.");
 		} else {
 			setLigado(false);
 			System.out.println(marca + " " + modelo + " foi desligado.");
+			acionarFreioDeMao(true);
 		}
 	}
 	
-	public void Acelerar() {
-		if (isLigado()) {
-			System.out.println(marca + " " + modelo + " esta acelerando.");
-		} else if (isAcelerando()) {
+	public void acelerar() {
+		if (isAcelerando()) {
 			System.out.println(marca + " " + modelo + " ja esta acelerando.");
+		} else if (isLigado()) {
+			acionarFreioDeMao(false);
+			setAcelerando(true);  // atualiza o valor do atributo acelerando
 		} else {
 			System.out.println("Para acelerar " + marca + " " + modelo + " ligue-o.");
 		}
 	}
 	
-	public void ReduzirParar() {
-		if (!isLigado()) {
+	public void reduzirParar() {
+		if (isLigado() && (isAcelerando() == false)) {  // se estiver ligado e sem acelerar (parado)
+			System.out.println(marca + " " + modelo + " nao pode reduzir, pois esta parado.");
+		} else if (!isLigado()){  // se estiver desligado não pode reduzir
 			System.out.println(marca + " " + modelo + " nao pode reduzir, pois esta parado.");
 		} else {
 			System.out.println(marca + " " + modelo + " esta reduzindo.");
 			setAcelerando(false);
 			System.out.println(marca + " " + modelo + " parou.");
+			acionarFreioDeMao(true);
 		} 	
 	}
 	
+	public void acionarFreioDeMao(boolean acionar) {
+		setFreioDeMao(acionar);
+	}
+	
 	// sera feito depois
-	public void Abastecer() {}  
+	public void abastecer() {}  
 	
 	public String getModelo() {
 		return modelo;
@@ -98,6 +112,7 @@ public class Carro {
 	
 	public void setLigado(boolean estaLigado) {
 		this.ligado = estaLigado;
+		System.out.println(marca + " " + modelo + " foi ligado.");
 	}
 	
 	public boolean isAcelerando() {
@@ -106,6 +121,20 @@ public class Carro {
 	
 	public void setAcelerando(boolean estaAcelerando) {
 		this.acelerando = estaAcelerando;
+		System.out.println(marca + " " + modelo + " esta acelerando.");
+	}
+	
+	public boolean isFreioDeMao() {
+		return freioDeMao;
+	}
+	
+	public void setFreioDeMao(boolean puxarFreio) {
+		this.freioDeMao = puxarFreio;
+		if (puxarFreio) {
+			System.out.println("O freio de mao foi ativado.");
+		} else {
+			System.out.println("O freio de mao foi desativado.");
+		}
 	}
 	
 	public int getAno() {
